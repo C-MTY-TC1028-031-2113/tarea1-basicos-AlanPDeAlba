a0b3a6ec25766720d9597e6d2ba97510227c12ee
Alan De Alba <A01177873@itesm.mx>
2021-08-22 17:13:33 -0500

    Tarea Pendiente Linea

--- assignments/03PendienteLinea/src/exercise.py
+++ assignments/03PendienteLinea/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
     #Lee los datos
+    primerax = float(input("Escribe x1: "))
+    primeray = float(input("Escribe y1: "))
+    segundax = float(input("Escribe x2: "))
+    segunday = float(input("Escribe y2: "))
+    pendiente = (segunday - primeray)/(segundax - primerax)
+    print("La pendiente es: " + str(pendiente))
     pass
 
 
