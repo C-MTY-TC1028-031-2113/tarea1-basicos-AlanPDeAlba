b57980aaee909a6dc21691d0af0641013d313af2
Alan De Alba <A01177873@itesm.mx>
2021-08-23 00:12:19 -0500

    Tarea Banco

--- assignments/05CuentaBancaria/src/exercise.py
+++ assignments/05CuentaBancaria/src/exercise.py
@@ -1,5 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
+    saldoanterior = float(input("Dame el saldo del mes anterior: "))
+    ingresos = float(input("Dame los ingresos: "))
+    egresos = float(input("Dame los egresos: "))
+    cheques = int(input("Dame el numero de cheques: "))
+    saldoactual = saldoanterior + ingresos - egresos - (cheques * 13)
+    total = saldoactual - (saldoactual * 0.075)
+    print("El saldo final de la cuenta es: " + str(total))
     pass
 
 
